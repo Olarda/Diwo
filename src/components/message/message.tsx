2ebc60c47d606dbe195b7a0c4fbd17916c188435
Olarda <dariafurzikova@yandex.ru>
2019-09-03 14:33:44 +0300

    chore: basic message styles

--- src/components/message/message.tsx
+++ src/components/message/message.tsx
@@ -1,22 +1,24 @@
 import * as React from 'react';
+import './message.css';
 import Date from "../date/date";
 
 interface MessageProps {
     title: string
     whoSent?: string
     message: string,
+    messageState: string
     date: number
 
 }
 
 function Message(props: MessageProps) {
-    const textMessage = `${props.whoSent + ':'}${props.message}`;
+    const textMessage = `${props.whoSent + ':'} ${props.message}`;
 
     return (
         <div className='message'>
             <div>{props.title}</div>
 
-            <div>
+            <div className= {`message-desc ${props.messageState}`}>
                 <div>{textMessage}</div>
                 <Date date={props.date}/>
             </div>
